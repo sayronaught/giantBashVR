fc7cb354162ee298a35ccd35e067f918a3590030
sayronaught <sayro_naught@hotmail.com>
2021-08-17 18:47:20 +0200

    hammer pathing

--- Assets/Scripts/hammerController.cs
+++ Assets/Scripts/hammerController.cs
@@ -17,6 +17,8 @@ public class hammerController : MonoBehaviour
     public bool rightPress;
     public bool leftPress;
 
+    public List<Vector3> rightHoldPositions;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,9 +41,19 @@ public class hammerController : MonoBehaviour
             device.IsPressed(InputHelpers.Button.TriggerPressed, out rightPress);
             device.IsPressed(InputHelpers.Button.TriggerPressed, out leftPress);
             if ( rightPress || leftPress )
-            {
+            { // press
                 //hammer.transform.position = leftHand.transform.position;
                 hammer.transform.position = rightHand.transform.position;
+                rightHoldPositions.Add(rightHand.transform.position);
+            } else { // not pressed
+                if ( rightHoldPositions.Count > 0) 
+                { // just released, have list of held positions
+                    // calculate hammer speed and trajectory
+                    // add to hammers rigidbody before releasing
+                    int framesBack = 10;
+                    if (framesBack < rightHoldPositions.Count) framesBack = rightHoldPositions.Count;
+                    rightHoldPositions.Clear();
+                }
             }
         }
     }
