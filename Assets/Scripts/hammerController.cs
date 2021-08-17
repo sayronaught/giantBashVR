ec1a8b897eff465f61fbc8761bc7e5d79b62a305
sayronaught <sayro_naught@hotmail.com>
2021-08-17 17:14:51 +0200

    Hammer SCript start

---
+++ Assets/Scripts/hammerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hammerController : MonoBehaviour
+{
+    public GameObject leftHand;
+    public GameObject rightHand;
+    public GameObject hammer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
