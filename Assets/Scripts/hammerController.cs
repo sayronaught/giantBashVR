e8a5d8236ce2bb5d185ef745f2069f9998e745ba
sayronaught <sayro_naught@hotmail.com>
2021-08-17 18:20:27 +0200

    Update hammerController.cs

--- Assets/Scripts/hammerController.cs
+++ Assets/Scripts/hammerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class hammerController : MonoBehaviour
 {
@@ -8,15 +10,37 @@ public class hammerController : MonoBehaviour
     public GameObject rightHand;
     public GameObject hammer;
 
+    public XRController controllerLeft;
+    public XRController controllerRight;
+    public XRRayInteractor interactorLeft;
+    public XRRayInteractor interactorRight;
+    public bool rightPress;
+    public bool leftPress;
+
     // Start is called before the first frame update
     void Start()
     {
+        controllerLeft = leftHand.GetComponent<XRController>();
+        controllerRight = rightHand.GetComponent<XRController>();
+        interactorLeft = leftHand.GetComponent<XRRayInteractor>();
+        interactorRight = rightHand.GetComponent<XRRayInteractor>();
+
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller & InputDeviceCharacteristics.TrackedDevice, _inputDevices);
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+            device.IsPressed(InputHelpers.Button.TriggerPressed, out rightPress);
+            if ( rightPress )
+            {
+                hammer.transform.position = leftHand.transform.position;
+            }
+        }
     }
 }
