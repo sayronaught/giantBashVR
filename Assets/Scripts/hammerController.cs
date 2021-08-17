0c31244112257b1616e85fc930b92fde1c771252
sayronaught <sayro_naught@hotmail.com>
2021-08-17 19:40:46 +0200

    button another test

--- Assets/Scripts/hammerController.cs
+++ Assets/Scripts/hammerController.cs
@@ -41,32 +41,24 @@ public class hammerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        righty.IsPressed(InputHelpers.Button.PrimaryButton,out rightPress);
+        lefty.IsPressed(InputHelpers.Button.Trigger,out leftPress);
+        righty.IsPressed(InputHelpers.Button.PrimaryButton, out rightPress);
         //InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller & InputDeviceCharacteristics.TrackedDevice, _inputDevices);
-        var inputDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-        foreach (var device in inputDevices)
-        {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-            //device.IsPressed(InputHelpers.Button.TriggerPressed, out rightPress);
-            //device.IsPressed(InputHelpers.Button.PrimaryButton, out leftPress);
-            //righty.IsPressed(InputHelpers.Button.PrimaryButton, out rightPress);
-            if ( rightPress )
-            { // press
-                //hammer.transform.position = leftHand.transform.position;
-                hammer.transform.position = rightHand.transform.position;
-                hammerRB.velocity = Vector3.zero;
-                hammerRB.angularVelocity = Vector3.zero;
-                rightHoldPositions.Add(rightHand.transform.position);
-            } else { // not pressed
-                if ( rightHoldPositions.Count > 0) 
-                { // just released, have list of held positions
-                    // calculate hammer speed and trajectory
-                    // add to hammers rigidbody before releasing
-                    int framesBack = 10;
-                    if (framesBack < rightHoldPositions.Count) framesBack = rightHoldPositions.Count;
-                    rightHoldPositions.Clear();
-                }
+        if (rightPress||leftPress)
+        { // press
+          //hammer.transform.position = leftHand.transform.position;
+            hammer.transform.position = rightHand.transform.position;
+            hammerRB.velocity = Vector3.zero;
+            hammerRB.angularVelocity = Vector3.zero;
+            rightHoldPositions.Add(rightHand.transform.position);
+        } else { // not pressed
+            if (rightHoldPositions.Count > 0)
+            { // just released, have list of held positions
+              // calculate hammer speed and trajectory
+              // add to hammers rigidbody before releasing
+                int framesBack = 10;
+                if (framesBack < rightHoldPositions.Count) framesBack = rightHoldPositions.Count;
+                rightHoldPositions.Clear();
             }
         }
     }
