bf3d9c3946fce6569f1c864797040db0cdb068d9
sayronaught <sayro_naught@hotmail.com>
2021-08-25 14:39:19 +0200

    Fixet packet id, and improved hammer physics

--- Assets/Scripts/hammerController.cs
+++ Assets/Scripts/hammerController.cs
@@ -17,6 +17,10 @@ public class hammerController : MonoBehaviour
     public bool rightPress;
     public bool leftPress;
 
+    public float magnetspeed;
+    public float magnetmultiplier=1.1f;
+    public float magnetminimum=1f;
+
     public List<Vector3> rightHoldPositions;
 
     public UnityEngine.XR.InputDevice lefty;
@@ -43,28 +47,37 @@ public class hammerController : MonoBehaviour
     void Update()
     {
         lefty.IsPressed(InputHelpers.Button.Trigger,out leftPress);
-        righty.IsPressed(InputHelpers.Button.PrimaryButton, out rightPress);
+        righty.IsPressed(InputHelpers.Button.Trigger, out rightPress);
+        //lefty.IsPressed(InputHelpers.Button.PrimaryButton, out leftPress);
+        //righty.IsPressed(InputHelpers.Button.PrimaryButton, out rightPress);
         //InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Controller & InputDeviceCharacteristics.TrackedDevice, _inputDevices);
         if ( rightPress )
         { // press
-          //hammer.transform.position = leftHand.transform.position;
             hammer.transform.position = rightHand.transform.position;
-            hammerRB.velocity = Vector3.zero;
-            hammerRB.angularVelocity = Vector3.zero;
+            //hammer.transform.position = Vector3.MoveTowards(hammer.transform.position, rightHand.transform.position, Time.deltaTime*magnetspeed*100000000000f);
+            hammerRB.isKinematic = true;
             rightHoldPositions.Add(rightHand.transform.position);
         } else if ( leftPress ) {
             hammer.transform.position = leftHand.transform.position;
-            hammerRB.velocity = Vector3.zero;
-            hammerRB.angularVelocity = Vector3.zero;
+            hammer.transform.rotation = leftHand.transform.rotation;
+            hammer.transform.Rotate(0, 90, 0);
+            hammerRB.isKinematic = true;
             rightHoldPositions.Add(leftHand.transform.position);
         } else { // not pressed
             if (rightHoldPositions.Count > 0)
             { // just released, have list of held positions
               // calculate hammer speed and trajectory
               // add to hammers rigidbody before releasing
-                int framesBack = 10;
+                
+                int framesBack = 100;
                 if (framesBack < rightHoldPositions.Count) framesBack = rightHoldPositions.Count;
+                Vector3 force = rightHoldPositions[rightHoldPositions.Count] - rightHoldPositions[rightHoldPositions.Count - framesBack];
+                hammerRB.velocity = Vector3.zero;
+                hammerRB.angularVelocity = Vector3.zero;
+                hammerRB.AddForce(force*1000000f);
+                hammerRB.isKinematic = false;
                 rightHoldPositions.Clear();
+                magnetspeed = magnetminimum;
             }
         }
     }
