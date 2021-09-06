b033bca0c8712481ae6c17a14bb41a4bfcc661fc
sayronaught <sayro_naught@hotmail.com>
2021-09-06 07:46:42 +0100

    4 gamestages setup
    0 tutorial
    1 approach
    2 game on
    3 post score

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class gameController : MonoBehaviour
 {
     public Image titlescreen;
+    public GameObject thePlayer;
+    public Transform posTutorial;
+    public Transform posGameOn;
 
     private float gamestageCountDown = 10f;
+    private int gameStage = 0; // 0 tutorial, 1 approach, 2 game on, 3 post score
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,17 @@ public class gameController : MonoBehaviour
         if ( gamestageCountDown < 0f )
         {
             titlescreen.color = new Color(1f,1f,1f, titlescreen.color.a-(Time.deltaTime*0.1f));
+        }
+        switch ( gameStage )
+        {
+            case 1: // approach
+            break;
+            case 2: // game on
+            break;
+            case 3: // post score
+            break;
+            default: // tutorial
+            break;
         }
     }
 }
