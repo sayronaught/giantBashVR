cd75bf643174adc349a6d0d1f90cbd1b2a2b2179
sayronaught <sayro_naught@hotmail.com>
2021-09-20 15:47:21 +0200

    last edits

--- Assets/Scripts/bigBossHitLocation.cs
+++ Assets/Scripts/bigBossHitLocation.cs
@@ -10,8 +10,8 @@ public class bigBossHitLocation : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag != "Hammer" || !bigBossScript.isAwake) return;
-        var dam = (bigBossScript.mainHC.chargeLightning*2f + 10f)*damageMultiplier;
-        bigBossScript.takeDamage(dam+ collision.rigidbody.velocity.magnitude);
+        var dam = (bigBossScript.mainHC.chargeLightning*3f + 5f)*damageMultiplier;
+        bigBossScript.takeDamage(dam+ (collision.rigidbody.velocity.magnitude*0.2f));
         bigBossScript.mainGC.maxSpeed = (int)collision.rigidbody.velocity.magnitude;
     }
 
