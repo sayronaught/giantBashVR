c668b24cdc226f40f59d2be0389b2147a002982d
HowskiTime <89068163+HowskiTime@users.noreply.github.com>
2021-10-26 13:09:04 +0200

    Dog

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    Rigidbody rb;
+    public bool chase;
+    public bool stop;
+    public GameObject dog;
+
+    public Animator anima;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anima = transform.GetChild(1).GetComponent<Animator>();         
+    }
+
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (chase == true)
+        {
+            //anim.animation
+            speed = 2;
+            Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+            rb.MovePosition(pos);
+            transform.LookAt(target);
+            anima.SetBool("isChasing", true);
+           
+
+        }
+        else
+        {
+            speed = 0;
+            anima.SetBool("isChasing", false);
+        }
+        
+
+
+
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        chase = false;
+        stop = true;
+        //anim.Play("metarig|iDLE");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        chase = true;
+        stop = false;
+        speed = 2;
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+        rb.MovePosition(pos);
+        transform.LookAt(target);
+        
+    }
+}
