dc9336f63b88cef9423f752c81a9bb777a6d01bd
sayronaught <sayro_naught@hotmail.com>
2021-11-13 05:50:54 +0100

    stable build + Reset button
    added scene for latest stable
    updated ftps benchmark script to inlcudes ftps on/off and reset button

--- Assets/Scripts/benchmark/benchmarkFPS.cs
+++ Assets/Scripts/benchmark/benchmarkFPS.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
 
 public class benchmarkFPS : MonoBehaviour
 {
@@ -14,6 +17,20 @@ public class benchmarkFPS : MonoBehaviour
     private float secondCount = 1f;
     private int framesThisSecond = 0;
 
+    public UnityEngine.XR.InputDevice leftController;
+    public UnityEngine.XR.InputDevice rightController;
+    public float updateControllerTimer = 0f;
+    private void updateController()
+    {
+        if (Application.isEditor) return;
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+        leftController = leftHandDevices[0];
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
+        rightController = rightHandDevices[0];
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +40,12 @@ public class benchmarkFPS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*currentFps = (int)(1f / Time.deltaTime);
-        if (Time.frameCount % 50 == 0)
-            fpsHud.text = currentFps.ToString() + "/" + worstFps.ToString() + " FPS";
-        */
+        updateControllerTimer -= Time.deltaTime;
+        if (updateControllerTimer < 0f)
+        {
+            updateController();
+            updateControllerTimer = 2f;
+        }
         secondCount -= Time.deltaTime;
         framesThisSecond++;
         if (secondCount > 0f) return;
@@ -37,5 +56,24 @@ public class benchmarkFPS : MonoBehaviour
             fpsHud.text = currentFps.ToString() + "/72 FPS\n+x" + rightHandPos.localPosition.x.ToString() + "\ny" + rightHandPos.localPosition.y.ToString() + "\nz" + rightHandPos.localPosition.z.ToString();
         else
             fpsHud.text = currentFps.ToString() + "/72 FPS";
+        bool debug = false;
+        rightController.IsPressed(InputHelpers.Button.SecondaryButton, out debug);
+        bool reset = false;
+        rightController.IsPressed(InputHelpers.Button.PrimaryButton, out reset);
+        if ( debug )
+        {// wants debug on or off
+            if ( fpsHud.gameObject.activeSelf )
+            {
+                fpsHud.gameObject.SetActive(false);
+            } else {
+                fpsHud.gameObject.SetActive(true);
+            }
+            
+        }
+        if ( reset )
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
     }
 }
