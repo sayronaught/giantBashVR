fda9ba7a8a821fdc90204c23bfa5714b974b2228
sayronaught <sayro_naught@hotmail.com>
2021-12-14 13:13:06 +0100

    Loadscreen dwarf preliminary

---
+++ Assets/Scripts/3dMenu/LoadScreen/loadDwarf.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadDwarf : MonoBehaviour
+{
+    public AudioSource sfAnvil;
+    public AudioSource sfDwarf;
+    public TextMesh vikingFact;
+
+    public AudioClip[] anvilSounds;
+    public string[] vikingFacts;
+
+    public void eventHitAnvil()
+    {
+        sfAnvil.clip = anvilSounds[Random.Range(0,anvilSounds.Length)];
+        sfAnvil.pitch = Random.Range(0.8f, 1.2f);
+        sfAnvil.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vikingFact.text = "Fact: "+vikingFacts[Random.Range(0, vikingFacts.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
