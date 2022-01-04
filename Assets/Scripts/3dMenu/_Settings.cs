8ecf45370e22ca498f1a20beb8688d216bad3c3e
sayronaught <sayro_naught@hotmail.com>
2022-01-04 12:32:59 +0100

    audiomixer & crows(sebastianH)

---
+++ Assets/Scripts/3dMenu/_Settings.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class _Settings : MonoBehaviour
+{
+
+    public AudioMixer masterMixer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad( gameObject );
+    }
+
+    private void Update()
+    {
+        //masterMixer.SetFloat("VolAmb", -80f);
+        //masterMixer.SetFloat("VolDia", -80f);
+        //masterMixer.SetFloat("VolMus", -80f);
+        //masterMixer.SetFloat("VolSfx", -80f);
+    }
+
+}
---
+++ Assets/Scripts/crows.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crows : MonoBehaviour
+{
+    public AudioSource myAS;
+
+    public AudioClip[] cawing;
+
+    public float cawTimer = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cawTimer -= Time.deltaTime;
+        if (cawTimer > 0f) return;
+        myAS.clip = cawing[Random.Range(0, cawing.Length)];
+        myAS.pitch = Random.Range(0.9f, 1.1f);
+        myAS.Play();
+        cawTimer = Random.Range(1.5f, 4f);
+    }
+}
