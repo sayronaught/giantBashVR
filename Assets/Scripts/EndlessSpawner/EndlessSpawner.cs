4220d8003d57bf56931f295f09a07a2aede3a839
sayronaught <sayro_naught@hotmail.com>
2022-01-10 13:54:33 +0100

    async fix

--- Assets/Scripts/EndlessSpawner/EndlessSpawner.cs
+++ Assets/Scripts/EndlessSpawner/EndlessSpawner.cs
@@ -40,28 +40,30 @@ public class EndlessSpawner : MonoBehaviour
 
     private int waitTimer(int min, int startTimer, float reduxModifier)
     {
-        return min;
+        return Mathf.Max(min,startTimer-(int)(time*reduxModifier));
     }
 
     private async void EnemyBossSpawner()
     {
-        while (true)
+        while (!Application.isEditor || Application.isPlaying)
         {
-            await Task.Delay(waitTimer(200,10000,0.0001f));
             randomSpawnPoint = Random.Range(0, 2);
             var spawn = Instantiate(enemyBoss, spawnPoints[randomSpawnPoint].position, Quaternion.identity);
             spawn.GetComponent<EnemyJotunBoss>().health = time * 5f;
+            Debug.Log("boss waiting: " + waitTimer(2000, 30000, 0.001f).ToString());
+            await Task.Delay(waitTimer(2000, 10000, 0.0001f));
         }
     }
 
     private async void EnemySpawner()
     {
-        while (true)
+        while (!Application.isEditor || Application.isPlaying)
         {
-            await Task.Delay(waitTimer(100, 5000, 0.001f));
             randomSpawnPoint = Random.Range(0, 2);
             var spawn = Instantiate(enemy[Random.Range(0,1)], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
             spawn.GetComponent<EnemyJotunBoss>().health = time * 0.5f;
+            Debug.Log("enemy waiting: " + waitTimer(1000, 5000, 0.01f).ToString());
+            await Task.Delay(waitTimer(1000, 5000, 0.001f));
         }
     }
 }
