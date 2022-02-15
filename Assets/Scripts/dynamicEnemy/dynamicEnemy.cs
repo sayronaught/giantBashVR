d8cb8c9baa7d45d27e9e41886123a2d7765981b1
sayronaught <sayro_naught@hotmail.com>
2022-02-15 14:53:28 +0100

    better dynamic enemy
    most of the basics of enemy script

--- Assets/Scripts/dynamicEnemy/dynamicEnemy.cs
+++ Assets/Scripts/dynamicEnemy/dynamicEnemy.cs
@@ -10,6 +10,7 @@ public class dynamicEnemy : MonoBehaviour
         public float mass = 1f;
         public float strength = 1f;
         public float speed = 1f;
+        public float maxSpeed = 1f;
         public float jumpForce = 1000f;
         public float damageReduction = 1f;
         public float maxHealth = 100f;
@@ -20,18 +21,19 @@ public class dynamicEnemy : MonoBehaviour
     public class animList
     {
         public string[] idles;
-        public string hurt;
-        public string death;
-        public string attackMeleeLight;
-        public string attackMeleeheavy;
-        public string attackRangeLight;
-        public string attackRangeHeavy;
+        public string[] hurt;
+        public string[] death;
+        public string[] attackMeleeLight;
+        public string[] attackMeleeheavy;
+        public string[] attackRangeLight;
+        public string[] attackRangeHeavy;
         public string[] conversations;
         public string[] taunts;
         public string[] surrenders;
     }
     public animList anims;
     public bool animIdle = true;
+    public GameObject instantDeathPrefab;
 
     [System.Serializable]
     public class soundList
@@ -48,22 +50,14 @@ public class dynamicEnemy : MonoBehaviour
     public soundList sounds;
 
     public float Hitpoints = 100f;
-    [System.Serializable]
-    public class aiAction
-    {
-        public string name;
-        public float percentageChance;
-        public float timeBeforeReevaluate;
-        public void set(string newName, float newPercent, float newTime) 
-        {
-            name = newName; percentageChance = newPercent; timeBeforeReevaluate = newTime;
-        }
-    }
-    public class aiActionTable
-    {
-        public aiAction attack;
-    }
-    public aiActionTable actionTable;
+
+    public Transform[] waypoints;
+
+    private int currentWaypoint;
+    private Transform finalWaypoint;
+    private Transform temporaryTarget;
+
+    private float lastActionDelay = 0f;
 
     private Animator myAnim;
     private AudioSource mySound;
@@ -73,17 +67,53 @@ public class dynamicEnemy : MonoBehaviour
         animIdle = true;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag != "Hammer") return;
+        if (Hitpoints < 0f)
+        {
+            if ( instantDeathPrefab )
+            { // remove model and add bloodsplat or other prefab
+                var blood = Instantiate(instantDeathPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                blood.transform.position = transform.position;
+                Destroy(gameObject);
+            } else { // play death anim
+                playRandomAnim(anims.death);
+                Destroy(gameObject, 10);
+            }
+        }   
+    }
+
+    void Awake()
     {
         Hitpoints = stats.maxHealth;
         myAnim = GetComponent<Animator>();
         mySound = GetComponent<AudioSource>();
+        float size = Random.Range(1f, 3f);
+        transform.localScale = new Vector3(size, size, size);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // look at target
+        if (temporaryTarget)
+            transform.LookAt(temporaryTarget);
+        else
+            transform.LookAt(finalWaypoint);
+
+    }
+
+    void playRandomAnim( string[] animlist )
+    {
+        if ( animlist.Length < 1 ) return;
+        myAnim.Play(animlist[Random.Range(0, animlist.Length)]);
+    }
+
+    void playRandomSound( AudioClip[] acList )
+    {
+        if ( acList.Length < 1 ) return;
+        mySound.clip = acList[Random.Range(0, acList.Length)];
+        mySound.Play();
     }
 }
