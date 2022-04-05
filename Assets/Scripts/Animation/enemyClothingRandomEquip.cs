f2874c91029831bff356c58fa552e279c645dfbc
Shenep <54121645+DispyExtra@users.noreply.github.com>
2022-04-05 11:30:19 +0200

    Bulky Jotunn Armor update
    - Bulky Jotunn now has randomized armor

--- Assets/Scripts/Animation/enemyClothingRandomEquip.cs
+++ Assets/Scripts/Animation/enemyClothingRandomEquip.cs
@@ -9,6 +9,11 @@ public class enemyClothingRandomEquip : MonoBehaviour
     public GameObject[] requiredLegClothing;
     public GameObject[] randomClothing;
 
+    public GameObject[] selectOne01;
+    public GameObject[] selectOne02;
+    public GameObject[] selectOne03;
+    public GameObject[] selectOne04;
+
     //Chance at a random clothing piece appearing in % (0-100)
     int randomChance = 35;
 
@@ -44,6 +49,35 @@ public class enemyClothingRandomEquip : MonoBehaviour
                     if (randomC != null)
                         randomC.SetActive(true);
             }
+
+        }
+
+        if (selectOne01.Length > 0)
+        {
+            GameObject selectOne01C = selectOne01[Random.Range(0, selectOne01.Length)];
+            if (selectOne01C != null)
+                selectOne01C.SetActive(true);
+
+        }
+        if (selectOne02.Length > 0)
+        {
+            GameObject selectOne02C = selectOne02[Random.Range(0, selectOne02.Length)];
+            if (selectOne02C != null)
+                selectOne02C.SetActive(true);
+
+        }
+        if (selectOne03.Length > 0)
+        {
+            GameObject selectOne03C = selectOne03[Random.Range(0, selectOne03.Length)];
+            if (selectOne03C != null)
+                selectOne03C.SetActive(true);
+
+        }
+        if (selectOne04.Length > 0)
+        {
+            GameObject selectOne04C = selectOne04[Random.Range(0, selectOne04.Length)];
+            if (selectOne04C != null)
+                selectOne04C.SetActive(true);
 
         }
 
