53b3dab59fcd7ded46564f75b41ccbc01cb7fe72
Shenep <54121645+DispyExtra@users.noreply.github.com>
2022-04-05 13:39:18 +0200

    Enemy Clothing Edits

--- Assets/Scripts/Animation/enemyClothingRandomEquip.cs
+++ Assets/Scripts/Animation/enemyClothingRandomEquip.cs
@@ -14,6 +14,9 @@ public class enemyClothingRandomEquip : MonoBehaviour
     public GameObject[] selectOne03;
     public GameObject[] selectOne04;
 
+
+    public GameObject[] deleteOnStartup;
+
     //Chance at a random clothing piece appearing in % (0-100)
     int randomChance = 35;
 
@@ -24,7 +27,19 @@ public class enemyClothingRandomEquip : MonoBehaviour
         {
             GameObject torsoC = torsoClothing[Random.Range(0, torsoClothing.Length)];
             if (torsoC != null)
+            {
                 torsoC.SetActive(true);
+
+            }
+            foreach (GameObject torso in torsoClothing)
+            {
+                if (torso == null)
+                    continue;
+                if (torso.activeSelf == false)
+                {
+                    Destroy(torso);
+                }
+            }
 
         }
 
@@ -38,6 +53,16 @@ public class enemyClothingRandomEquip : MonoBehaviour
             if (legC != null)
                 legC.SetActive(true);
 
+            foreach (GameObject legs in requiredLegClothing)
+            {
+                if (legs == null)
+                    continue;
+                if (legs.activeSelf == false)
+                {
+                    Destroy(legs);
+                }
+            }
+
         }
 
         if (randomClothing.Length > 0)
@@ -46,8 +71,17 @@ public class enemyClothingRandomEquip : MonoBehaviour
             {
                 int randomNumber = Random.Range(0,100);
                 if(randomChance > randomNumber)
+                {
                     if (randomC != null)
                         randomC.SetActive(true);
+
+                }
+                else
+                {
+                    if (randomC != null)
+                        Destroy(randomC);
+
+                }
             }
 
         }
@@ -58,12 +92,32 @@ public class enemyClothingRandomEquip : MonoBehaviour
             if (selectOne01C != null)
                 selectOne01C.SetActive(true);
 
+            foreach (GameObject s1 in selectOne01)
+            {
+                if (s1 == null)
+                    continue;
+                if (s1.activeSelf == false)
+                {
+                    Destroy(s1);
+                }
+            }
+
         }
         if (selectOne02.Length > 0)
         {
             GameObject selectOne02C = selectOne02[Random.Range(0, selectOne02.Length)];
             if (selectOne02C != null)
                 selectOne02C.SetActive(true);
+
+            foreach (GameObject s2 in selectOne02)
+            {
+                if (s2 == null)
+                    continue;
+                if (s2.activeSelf == false)
+                {
+                     Destroy(s2);
+                }
+            }
 
         }
         if (selectOne03.Length > 0)
@@ -72,12 +126,46 @@ public class enemyClothingRandomEquip : MonoBehaviour
             if (selectOne03C != null)
                 selectOne03C.SetActive(true);
 
+            foreach (GameObject s3 in selectOne03)
+            {
+                if (s3 == null)
+                    continue;
+                if (s3.activeSelf == false)
+                {
+                    Destroy(s3);
+                }
+            }
+
         }
         if (selectOne04.Length > 0)
         {
             GameObject selectOne04C = selectOne04[Random.Range(0, selectOne04.Length)];
             if (selectOne04C != null)
                 selectOne04C.SetActive(true);
+
+            foreach (GameObject s4 in selectOne04)
+            {
+                if (s4 == null)
+                    continue;
+                if (s4.activeSelf == false)
+                {
+                    Destroy(s4);
+                }
+            }
+
+        }
+
+        if (deleteOnStartup.Length > 0)
+        {
+            foreach (GameObject deleteStartup in deleteOnStartup)
+            {
+                if (deleteStartup == null)
+                    continue;
+                if (deleteStartup.activeSelf == false)
+                {
+                    Destroy(deleteStartup);
+                }
+            }
 
         }
 
