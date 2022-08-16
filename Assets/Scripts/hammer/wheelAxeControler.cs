0cb7febbd76249e9a5631509b41ebabfc36db760
infernerner <101186811+infernerner@users.noreply.github.com>
2022-08-16 11:24:11 +0200

    possible hitbox fix

--- Assets/Scripts/hammer/wheelAxeControler.cs
+++ Assets/Scripts/hammer/wheelAxeControler.cs
@@ -47,11 +47,13 @@ public class wheelAxeControler : MonoBehaviour
                 myAS.volume = 1f;
                 
             }
+            
             if ( spinning )
             {
                 myAS.pitch = 0.2f + (Mathf.Abs(myRB.velocity.magnitude) * 0.025f);
                 transform.Rotate(Vector3.left, -150f * Time.deltaTime * (myRB.velocity.magnitude*3f));  
             }
+            
             if (myHC.beingSummoned())
             {
                 spinning = false;
@@ -59,38 +61,66 @@ public class wheelAxeControler : MonoBehaviour
             }
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    /* private void OnCollisionEnter(Collision collision)
+      {
+          spinning = false;
+          myAS.volume = 0f;
+          if (collision.gameObject.tag == "WheelTarget" )
+          { 
+              //if (!collision.collider.bounds.Contains(collision.GetContact(0).point)) return;
+              //if (!myBC.bounds.Contains(collision.GetContact(0).point)) return;
+
+              var axeshadow = Instantiate(axeprefab, transform.position, transform.rotation);
+              transform.position = axeposition.position;
+              transform.rotation = axeposition.rotation;
+              if ( collision.transform.name == "targetCollider")
+                  axeshadow.transform.SetParent(collision.transform.parent);
+              else
+                  axeshadow.transform.SetParent(collision.transform);
+              axeshadow.transform.localScale = new Vector3(5f, 5f, 5f);
+              myRB.velocity = Vector3.zero;
+              myRB.angularVelocity = Vector3.zero;
+              Destroy(axeshadow, 30f);
+              Axes++;
+              Axecounter.text = Axes.ToString();
+              var fireball = Instantiate(fireballPrefab, collision.GetContact(0).point, Quaternion.identity);
+              fireball.transform.SetParent(collision.transform);
+              fireball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+              Destroy(fireball, 30f);
+              /* Debug with:
+              if (hitToTest.collider.bounds.Contains(telePosition))
+              {
+                  print("point is inside collider");
+              }
+              */ /*
+          }
+      } */
+    private void OnTriggerEnter(Collider other)
     {
         spinning = false;
         myAS.volume = 0f;
-        if (collision.gameObject.tag == "WheelTarget" )
-        { 
-            if (!collision.collider.bounds.Contains(collision.GetContact(0).point)) return;
-            if (!myBC.bounds.Contains(collision.GetContact(0).point)) return;
+        if (other.gameObject.tag == "WheelTarget")
+        {
+            //if (!collision.collider.bounds.Contains(collision.GetContact(0).point)) return;
+            //if (!myBC.bounds.Contains(collision.GetContact(0).point)) return;
 
             var axeshadow = Instantiate(axeprefab, transform.position, transform.rotation);
             transform.position = axeposition.position;
             transform.rotation = axeposition.rotation;
-            if ( collision.transform.name == "targetCollider")
-                axeshadow.transform.SetParent(collision.transform.parent);
+            if (other.transform.name == "targetCollider")
+                axeshadow.transform.SetParent(other.transform.parent);
             else
-                axeshadow.transform.SetParent(collision.transform);
+                axeshadow.transform.SetParent(other.transform);
             axeshadow.transform.localScale = new Vector3(5f, 5f, 5f);
             myRB.velocity = Vector3.zero;
             myRB.angularVelocity = Vector3.zero;
             Destroy(axeshadow, 30f);
             Axes++;
             Axecounter.text = Axes.ToString();
-            var fireball = Instantiate(fireballPrefab, collision.GetContact(0).point, Quaternion.identity);
-            fireball.transform.SetParent(collision.transform);
+            /*var fireball = Instantiate(fireballPrefab, other.GetContact(0).point, Quaternion.identity);
+            fireball.transform.SetParent(other.transform);
             fireball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-            Destroy(fireball, 30f);
-            /* Debug with:
-            if (hitToTest.collider.bounds.Contains(telePosition))
-            {
-                print("point is inside collider");
-            }
-            */
+            Destroy(fireball, 30f);*/
         }
     }
 }
