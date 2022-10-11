8e4c4ca9a6f6042addfbc1a07ac07f5dc1fc5b98
infernerner <101186811+infernerner@users.noreply.github.com>
2022-10-11 12:21:56 +0200

    mini game clean up

--- Assets/Scripts/miniGames/pinTheAxe/BackgroundDancer.cs
+++ Assets/Scripts/miniGames/pinTheAxe/BackgroundDancer.cs
@@ -6,28 +6,30 @@ public class BackgroundDancer : MonoBehaviour
 {
     Rigidbody myRB;
     CapsuleCollider myCC;
-    Animation myAni;
-    
+    Animator myAni;
+    public GameObject bloodSplat;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
         myCC = GetComponent<CapsuleCollider>();
-        myAni = GetComponent<Animation>();
+        myAni = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Hammer")
         {
-            myAni.Play();
+
+            myAni.Play("Armature_002|Death");
+            Instantiate(bloodSplat, transform.position, Quaternion.identity);
         }
     }
 }
