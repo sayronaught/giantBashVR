c4be16d02a70a6e49cfb009a570cd4d5c40e2774
infernerner <101186811+infernerner@users.noreply.github.com>
2022-10-25 14:02:01 +0200

    updated kill zone

--- Assets/Scripts/hammer/wheelAxeControler.cs
+++ Assets/Scripts/hammer/wheelAxeControler.cs
@@ -62,41 +62,16 @@ public class wheelAxeControler : MonoBehaviour
                 myAS.volume = 0f;
             }
         }
+        if (transform.position.y <= -10)
+        {
+            transform.position = axeposition.position;
+            transform.rotation = axeposition.rotation;
+            myRB.velocity = Vector3.zero;
+            myRB.angularVelocity = Vector3.zero;
+            Axes++;
+            Axecounter.text = Axes.ToString();
+        }
     }
-     /*private void OnCollisionEnter(Collision collision)
-      {
-          spinning = false;
-          myAS.volume = 0f;
-          if (collision.gameObject.tag == "WheelTarget" )
-          { 
-              //if (!collision.collider.bounds.Contains(collision.GetContact(0).point)) return;
-              //if (!myBC.bounds.Contains(collision.GetContact(0).point)) return;
-
-              var axeshadow = Instantiate(axeprefab, transform.position, transform.rotation);
-              transform.position = axeposition.position;
-              transform.rotation = axeposition.rotation;
-              if ( collision.transform.name == "targetCollider")
-                  axeshadow.transform.SetParent(collision.transform.parent);
-              else
-                  axeshadow.transform.SetParent(collision.transform);
-              axeshadow.transform.localScale = new Vector3(5f, 5f, 5f);
-              myRB.velocity = Vector3.zero;
-              myRB.angularVelocity = Vector3.zero;
-              Destroy(axeshadow, 30f);
-              Axes++;
-              Axecounter.text = Axes.ToString();
-              var fireball = Instantiate(fireballPrefab, collision.GetContact(0).point, Quaternion.identity);
-              fireball.transform.SetParent(collision.transform);
-              fireball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-              Destroy(fireball, 30f);
-              Debug with:
-              if (hitToTest.collider.bounds.Contains(telePosition))
-              {
-                  print("point is inside collider");
-              }
-              
-          }
-      } */
     private void OnTriggerEnter(Collider other)
     {
         spinning = false;
