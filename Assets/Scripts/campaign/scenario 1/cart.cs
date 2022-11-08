5b2fdc0afd9c742c0c461996de05fdf45dc977a3
infernerner <101186811+infernerner@users.noreply.github.com>
2022-11-08 12:46:51 +0100

    Update cart.cs

--- Assets/Scripts/campaign/scenario 1/cart.cs
+++ Assets/Scripts/campaign/scenario 1/cart.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class cart : MonoBehaviour
 {
-    [Tooltip("constant move speed of the player")]
-    public float cartSpeed = 0.4f;
-    [Tooltip("size of the waypoints")]
-    public float waypointHitbox = 0.5f;
-    [Tooltip("list of waypoints (counts form 0 and up)")]
-    public Transform[] cartWaypoints;
-    [Tooltip("current waypoint its moving to on the waypoint list (counts form 0 and up)")]
-    public int waypoint = 0;
-    
+   [Tooltip("constant move speed of the player")]
+   public float cartSpeed = 0.4f;
+   [Tooltip("size of the waypoints")]
+   public float waypointHitbox = 0.5f;
+
+
+    private bool move = true;
+    private float waitTime = 0;
+
+    [System.Serializable]
+
+    public class waypoint
+    {
+        [Tooltip("waypoint")]
+        public Transform whereToGo;
+        [Tooltip("should it stop?")]
+        public bool stopEvent = false;
+        [Tooltip("how long should it stop?")]
+        public float waitTime = 3f;
+    }
+    [Tooltip("list of waypoint (counts from 0)")]
+    public waypoint[] waypoints;
+    [Tooltip("current waypoint on the list")]
+    public int waypointInt = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +38,28 @@ public class cart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * cartSpeed;
+        if (move)
+        {
+            transform.position += transform.forward * Time.deltaTime * cartSpeed;
+        
+            Vector3 relativePos = waypoints[waypointInt].whereToGo.position - transform.position;
+            Quaternion toRotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 1 * Time.deltaTime);
+        }
+        if (!move)
+        {
+            waitTime -= Time.deltaTime;
+            if (waitTime < 0) move = true;
+        }
 
-        Vector3 relativePos = cartWaypoints[waypoint].position - transform.position;
-        Quaternion toRotation = Quaternion.LookRotation(relativePos);
-        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 1 * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, cartWaypoints[waypoint].position) <= waypointHitbox && waypoint < cartWaypoints.Length -1f) waypoint++;
+        if (Vector3.Distance(transform.position, waypoints[waypointInt].whereToGo.position) <= waypointHitbox && waypointInt < waypoints.Length - 1f)
+        {
+            if (waypoints[waypointInt].stopEvent)
+            {
+                move = false;
+                waitTime = waypoints[waypointInt].waitTime;
+            }
+            waypointInt++;
+        }
     }
 }
