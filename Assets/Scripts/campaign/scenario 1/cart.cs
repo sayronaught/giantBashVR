e92bd572ad50f3b0205b77f2864f08d12491c775
infernerner <101186811+infernerner@users.noreply.github.com>
2022-11-08 10:22:10 +0100

    cart waypoint system
    with new and shiny tool tips

---
+++ Assets/Scripts/campaign/scenario 1/cart.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cart : MonoBehaviour
+{
+    [Tooltip("constant move speed of the player")]
+    public float cartSpeed = 0.4f;
+    [Tooltip("size of the waypoints")]
+    public float waypointHitbox = 0.5f;
+    [Tooltip("list of waypoints (counts form 0 and up)")]
+    public Transform[] cartWaypoints;
+    [Tooltip("current waypoint its moving to on the waypoint list (counts form 0 and up)")]
+    public int waypoint = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * cartSpeed;
+
+        Vector3 relativePos = cartWaypoints[waypoint].position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 1 * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, cartWaypoints[waypoint].position) <= waypointHitbox) waypoint++;
+    }
+}
